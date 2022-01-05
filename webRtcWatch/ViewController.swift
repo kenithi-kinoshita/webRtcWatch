de2008ab5716c5f00d7b186428ba7e0db54e94b6
k-kinoshita <kinoken5939@gmail.com>
2022-01-05 11:59:49 +0900

    first commit

---
+++ webRtcWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webRtcWatch
+//
+//  Created by 木下健一 on 2022/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
